0310df4ac41737036d2361b4678b6f89ba8d0fdd
John Goff <gygn.johngoff@gmail.com>
2017-09-26 21:01:12 -0300

    Print family records instead of list of individuals

--- server.go
+++ server.go
@@ -7,7 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-  "os"
+	"os"
 )
 
 func printGedcom(w http.ResponseWriter, r *http.Request) {
@@ -23,29 +23,36 @@ func printGedcom(w http.ResponseWriter, r *http.Request) {
 		log.Fatal("Error decoding GEDCOM:", err)
 	}
 
-	for _, rec := range g.Individual {
-		if len(rec.Name) > 0 {
-			fmt.Fprintln(w, rec.Name[0].Name)
+	for _, rec := range g.Family {
+		fmt.Fprintln(w, rec.Xref)
+		fmt.Fprintln(w, "Husband: "+rec.Husband.Name[0].Name)
+		fmt.Fprintln(w, "Wife: "+rec.Wife.Name[0].Name)
+		fmt.Fprintln(w, "Children:")
+		for _, child := range rec.Child {
+			if len(child.Name) > 0 {
+				fmt.Fprintln(w, child.Name[0].Name)
+			}
 		}
+		fmt.Fprintln(w, "")
 	}
 }
 
 func main() {
 	http.HandleFunc("/", printGedcom)
-  var port string
-  ch := make(chan byte, 1)
-  if p := os.Getenv("PORT"); p != "" {
-    port = p
-  } else {
-    port = "9090"
-  }
+	var port string
+	ch := make(chan byte, 1)
+	if p := os.Getenv("PORT"); p != "" {
+		port = p
+	} else {
+		port = "9090"
+	}
 	go func() {
-    err := http.ListenAndServe(":" + port, nil)
-	  if err != nil {
-	  	log.Fatal("ListenAndServe:", err)
-	  }
-    ch <- 1
-  }()
-  fmt.Println("Listening on port :" + port)
-  <-ch
+		err := http.ListenAndServe(":"+port, nil)
+		if err != nil {
+			log.Fatal("ListenAndServe:", err)
+		}
+		ch <- 1
+	}()
+	fmt.Println("Listening on port :" + port)
+	<-ch
 }
