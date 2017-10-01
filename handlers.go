2ca1256f1d4a47dc3e9074a17a89668519b05d89
John Goff <gygn.johngoff@gmail.com>
2017-10-01 18:09:43 -0300

    Begin fleshing out API with families and individuals

---
+++ handlers.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/husobee/vestigo"
+	"github.com/iand/gedcom"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var g = parseGedcomFile()
+
+func parseGedcomFile() *gedcom.Gedcom {
+	data, err := ioutil.ReadFile("assets/gedcom/sample.ged")
+	if err != nil {
+		log.Fatal("Error reading file:", err)
+	}
+
+	d := gedcom.NewDecoder(bytes.NewReader(data))
+
+	g, err := d.Decode()
+	if err != nil {
+		log.Fatal("Error decoding GEDCOM:", err)
+	}
+
+	return g
+}
+
+func fixName(name string) string {
+	return strings.Replace(name, "/", "", -1)
+}
+
+func familyList(w http.ResponseWriter, r *http.Request) {
+	for _, rec := range g.Family {
+		fmt.Fprintf(w, "%s: %s & %s\n", rec.Xref, fixName(rec.Husband.Name[0].Name), fixName(rec.Wife.Name[0].Name))
+	}
+}
+
+func family(w http.ResponseWriter, r *http.Request) {
+	for _, rec := range g.Family {
+		if id := vestigo.Param(r, "id"); !strings.EqualFold(id, rec.Xref) {
+			continue
+		}
+
+		fmt.Fprintln(w, rec.Xref)
+		fmt.Fprintln(w, "Husband: "+fixName(rec.Husband.Name[0].Name))
+		fmt.Fprintln(w, "Wife: "+fixName(rec.Wife.Name[0].Name))
+		fmt.Fprintln(w, "Children:")
+		for _, child := range rec.Child {
+			if len(child.Name) > 0 {
+				fmt.Fprintln(w, fixName(child.Name[0].Name))
+			}
+		}
+		fmt.Fprintln(w, "")
+	}
+}
+
+func individualList(w http.ResponseWriter, r *http.Request) {
+	for _, rec := range g.Individual {
+		fmt.Fprintf(w, "%s: %s\n", rec.Xref, fixName(rec.Name[0].Name))
+	}
+}
+
+func individual(w http.ResponseWriter, r *http.Request) {
+	for _, rec := range g.Individual {
+		if id := vestigo.Param(r, "id"); !strings.EqualFold(id, rec.Xref) {
+			continue
+		}
+
+		fmt.Fprintln(w, rec.Xref)
+		fmt.Fprintln(w, fixName(rec.Name[0].Name))
+		fmt.Fprintln(w, rec.Sex)
+		for _, evnt := range rec.Event {
+			fmt.Fprintln(w, evnt.Tag)
+			fmt.Fprintln(w, evnt.Date)
+			fmt.Fprintln(w, evnt.Place.Name)
+		}
+		for _, attr := range rec.Attribute {
+			fmt.Fprintln(w, attr.Tag)
+			fmt.Fprintln(w, attr.Value)
+		}
+		fmt.Fprintln(w, "")
+		fmt.Fprintln(w, "Parents: ")
+		fmt.Fprintln(w, fixName(rec.Family[0].Family.Husband.Name[0].Name))
+		fmt.Fprintln(w, fixName(rec.Family[0].Family.Wife.Name[0].Name))
+		fmt.Fprintln(w, "")
+		fmt.Fprintln(w, "Children:")
+		for _, child := range rec.Family[0].Family.Child {
+			if len(child.Name) > 0 {
+				fmt.Fprintln(w, fixName(child.Name[0].Name))
+			}
+		}
+		fmt.Fprintln(w, "")
+	}
+}
--- server.go
+++ server.go
@@ -1,42 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"github.com/husobee/vestigo"
-	"github.com/iand/gedcom"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
-
-func printGedcom(w http.ResponseWriter, r *http.Request) {
-	data, err := ioutil.ReadFile("assets/gedcom/sample.ged")
-	if err != nil {
-		log.Fatal("Error reading file:", err)
-	}
-
-	d := gedcom.NewDecoder(bytes.NewReader(data))
-
-	g, err := d.Decode()
-	if err != nil {
-		log.Fatal("Error decoding GEDCOM:", err)
-	}
-
-	for _, rec := range g.Family {
-		fmt.Fprintln(w, rec.Xref)
-		fmt.Fprintln(w, "Husband: "+rec.Husband.Name[0].Name)
-		fmt.Fprintln(w, "Wife: "+rec.Wife.Name[0].Name)
-		fmt.Fprintln(w, "Children:")
-		for _, child := range rec.Child {
-			if len(child.Name) > 0 {
-				fmt.Fprintln(w, child.Name[0].Name)
-			}
-		}
-		fmt.Fprintln(w, "")
-	}
-}
 
 func main() {
 	ch := make(chan byte, 1)
@@ -52,8 +22,10 @@ func main() {
 	// ROUTES //
 	////////////
 	router.Get("/", http.FileServer(http.Dir("./assets")).ServeHTTP)
-	router.Get("/api/individual/:id", printGedcom)
-	router.Get("/api/family/:id", printGedcom)
+	router.Get("/api/individual", individualList)
+	router.Get("/api/individual/:id", individual)
+	router.Get("/api/family", familyList)
+	router.Get("/api/family/:id", family)
 
 	//run server in goroutine to print after the server has started
 	go func() {
