213e369e9f43a9b5606dbed14361f5ce47c812eb
John Goff <gygn.johngoff@gmail.com>
2017-10-02 13:39:24 -0400

    added some comments and converted individual record to JSON

--- handlers.go
+++ handlers.go
@@ -29,12 +29,18 @@ func parseGedcomFile() *gedcom.Gedcom {
 	return g
 }
 
+/** fixName
+	* Properly formats names to remove slashes and escape quotation marks
+ **/
 func fixName(name string) string {
 	fixed := strings.Replace(name, "/", "", -1)
-	fixed = strings.Replace(fixed, "\"", "\\\"", -1)
+	fixed = strings.Replace(fixed, `"`, `\"`, -1)
 	return fixed
 }
 
+/** familyList
+	* writes JSON representing the entire list of families from GEDCOM
+ **/
 func familyList(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "{")
 	for i, rec := range g.Family {
@@ -74,6 +80,9 @@ func family(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+/** individualList
+	* writes JSON representing the entire list of individuals from GEDCOM
+ **/
 func individualList(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "{")
 	for i, rec := range g.Individual {
@@ -85,35 +94,60 @@ func individualList(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "}")
 }
 
+/** individual
+	* writes JSON containing the information about an individual
+	* to the client.
+ **/
 func individual(w http.ResponseWriter, r *http.Request) {
 	for _, rec := range g.Individual {
 		if id := vestigo.Param(r, "id"); !strings.EqualFold(id, rec.Xref) {
 			continue
 		}
 
-		fmt.Fprintln(w, rec.Xref)
-		fmt.Fprintln(w, fixName(rec.Name[0].Name))
-		fmt.Fprintln(w, rec.Sex)
-		for _, evnt := range rec.Event {
-			fmt.Fprintln(w, evnt.Tag)
-			fmt.Fprintln(w, evnt.Date)
-			fmt.Fprintln(w, evnt.Place.Name)
+		fmt.Fprintln(w, "{")
+		fmt.Fprintln(w, `"id":"`+rec.Xref+`",`)
+		fmt.Fprintln(w, `"name":"`+fixName(rec.Name[0].Name)+`",`)
+		fmt.Fprintln(w, `"sex":"`+rec.Sex+`",`)
+		fmt.Fprintln(w, `"events":[`)
+		for i, evnt := range rec.Event {
+			if i != 0 {
+				fmt.Fprint(w, ",")
+			}
+			fmt.Fprintln(w, "{")
+			fmt.Fprintln(w, `"event":"`+evnt.Tag+`",`)
+			fmt.Fprintln(w, `"date":"`+evnt.Date+`",`)
+			fmt.Fprintln(w, `"place":"`+evnt.Place.Name+`"`)
+			fmt.Fprintln(w, "}")
 		}
-		for _, attr := range rec.Attribute {
-			fmt.Fprintln(w, attr.Tag)
-			fmt.Fprintln(w, attr.Value)
+		fmt.Fprintln(w, `],`)
+		fmt.Fprintln(w, `"attributes":[`)
+		for i, attr := range rec.Attribute {
+			if i != 0 {
+				fmt.Fprint(w, ",")
+			}
+			fmt.Fprintln(w, "{")
+			fmt.Fprintln(w, `"tag":"`+attr.Tag+`",`)
+			fmt.Fprintln(w, `"value":"`+attr.Value+`"`)
+			fmt.Fprintln(w, "}")
 		}
-		fmt.Fprintln(w, "")
-		fmt.Fprintln(w, "Parents: ")
-		fmt.Fprintln(w, fixName(rec.Family[0].Family.Husband.Name[0].Name))
-		fmt.Fprintln(w, fixName(rec.Family[0].Family.Wife.Name[0].Name))
-		fmt.Fprintln(w, "")
-		fmt.Fprintln(w, "Children:")
-		for _, child := range rec.Family[0].Family.Child {
-			if len(child.Name) > 0 {
-				fmt.Fprintln(w, fixName(child.Name[0].Name))
+		fmt.Fprintln(w, `]`)
+		fmt.Fprintln(w, `,"parents":[`)
+		if len(rec.Parents) > 0 {
+			fmt.Fprintln(w, `"`+fixName(rec.Parents[0].Family.Husband.Name[0].Name)+`",`)
+			fmt.Fprintln(w, `"`+fixName(rec.Parents[0].Family.Wife.Name[0].Name)+`"`)
+		}
+		fmt.Fprintln(w, "],")
+		fmt.Fprintln(w, `"children":[`)
+		if len(rec.Family) > 0 {
+			for i, child := range rec.Family[0].Family.Child {
+				if len(child.Name) > 0 {
+					if i != 0 {
+						fmt.Fprint(w, ",")
+					}
+					fmt.Fprintln(w, `"`+fixName(child.Name[0].Name)+`"`)
+				}
 			}
 		}
-		fmt.Fprintln(w, "")
+		fmt.Fprintln(w, "]}")
 	}
 }
